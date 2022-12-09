cea9a0adc6d115aeffa16d1001ea8de95b683fd2
Ignacio Titos <94903375+ignaciotitos@users.noreply.github.com>
2022-12-09 10:10:54 +0100

    feat: assignment of an employee on Vacaciones.go
    Para poder hacer Empleado un objeto valor he cambiado algunas variables que se pueden establecer en esta clase y las he cambiado para manejarlas aquí.
    
    Issues: #15 #16

--- src/Vacaciones.go
+++ src/Vacaciones.go
@@ -3,9 +3,16 @@ package src
 import (
 	fmt
 	time
+	Empleado
 )
 
 type Vacaciones struct{
 	Dias uint
 	Periodo []time.Time
+	type Worker struct{
+		Employee Empleado
+		Antiguedad uint
+		peticiones uint
+		VacacionesPedidas bool
+	}
 }
