3a0498cd2e25693b9431f05e89571827048dbfaa
ignaciotitos <titosignacio8@gmail.com>
2022-12-21 17:00:17 +0100

    change: mala sintaxis al declarar la clase

--- internal/Manejo.go
+++ internal/Manejo.go
@@ -3,13 +3,13 @@ package internal
 type Tarea uint8
 
 const (
-	Facil Tarea = 0
-	Medio       = 1
-	Dificil     = 2
-	Urgente 	= 3
+	Facil   Tarea = 0
+	Medio         = 1
+	Dificil       = 2
+	Urgente       = 3
 )
 
-type Struct manejar{
-	Tareas []Tarea
+type Manejar struct {
+	Tareas     []Tarea
 	Peticiones Peticiones
 }
