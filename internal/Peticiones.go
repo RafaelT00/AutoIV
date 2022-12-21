453149338c4121637158c2255e4bcddabdabcbe4
ignaciotitos <titosignacio8@gmail.com>
2022-12-21 17:00:53 +0100

    change: mala sintaxis y mala práctica en go

--- internal/Peticiones.go
+++ internal/Peticiones.go
@@ -5,13 +5,13 @@ import "time"
 type Season uint8
 
 const (
-	Invierno Season = 0
-	Primavera 		= 1
-	Verano 			= 2
-	Otonio			= 3
+	Invierno  Season = 0
+	Primavera        = 1
+	Verano           = 2
+	Otonio           = 3
 )
 
-type Peticiones struct{
-	DiasQueQuiero []Time.time
-	Temporada Season
+type Peticiones struct {
+	DiasQueQuiero []time.Time
+	Temporada     Season
 }
